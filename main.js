56c9ed645a1b21790ca43581779275269788b4a0
Michael C <michael@mchang.name>
2020-02-16 18:33:40 -0500

    updated dependencies, cleaned up functions, switched to enve from json, updated packagejson

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
 // imports
 const Discord = require('discord.js');
-const config = require('./config.json');
+require('enve');
 
 // create client
 const client = new Discord.Client();
 
 client.on('ready', () => {
-	console.log('Ready');
-	// set presence
-	client.user.setPresence({game: {name: config.name}, status: config.status})
+    console.log('Ready');
+    // set presence
+    client.user.setPresence({
+        game: {
+            name: process.enve.NAME
+        },
+        status: process.enve.STATUS
+    })
 });
 
+// trigger on message
 client.on('message', message => {
-    // check for message sent by author
-	if (config.users.indexOf(message.author.id) >=0) {
-        // delete message
-        message.delete();
-        // loglevel 1
-        if (config.loglevel >= 1) {
-            console.log(`${message.createdAt} - Deleted message from ${message.author.username}`);
-        }
-        // loglevel 2
-        if (config.loglevel >=2) {
-            console.log(`message - ${message.content}`);
-        }
-    }
+    checkMessage(message);
 });
 
 // login
-client.login(config.token);
+client.login(process.enve.TOKEN);
+
+
+function logMessage(message) {
+    // check log level and log accordingly
+    // loglevel 1 and above
+    if ( process.enve.level >= 1 ) {
+        console.log(`${message.createdAt} - Deleted message from ${message.author.username}`);
+    }
+    // loglevel 2
+    if ( process.enve.level == 2 ) {
+        console.log(`message - ${message.content}`);
+    }
+}
+
+function checkMessage(message) {
+    // check for message sent by author
+    if (process.enve.USERS.includes(message.author.id)) {
+        // delete message
+        message.delete();
+        // log message
+        logMessage(message)
+    }
+}
