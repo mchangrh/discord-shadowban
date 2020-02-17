962969f1ccae99c82fad93f9c6943229c8e3a448
Michael C <michael@mchang.name>
2020-02-16 19:17:37 -0500

    added commonJS linting

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
 // imports
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 require('dotenv').config()
-require('enve');
+require('enve')
 
 // create client
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 client.on('ready', () => {
-    console.log('Ready');
-    // set presence
-    client.user.setPresence({
-        game: {
-            name: process.enve.NAME
-        },
-        status: process.enve.STATUS
-    })
-});
+  console.log('Ready')
+  // set presence
+  client.user.setPresence({
+    game: {
+      name: process.enve.NAME
+    },
+    status: process.enve.STATUS
+  })
+})
 
 // trigger on message
 client.on('message', message => {
-    checkMessage(message);
-});
+  checkMessage(message)
+})
 
 // login
-client.login(process.enve.TOKEN);
+client.login(process.enve.TOKEN)
 
-function logMessage(message) {
-    // check log level and log accordingly
-    // loglevel 1 and above
-    if ( process.enve.LOGLEVEL >= 1 ) {
-        console.log(`${message.createdAt} - Deleted message from ${message.author.username}`);
-    }
-    // loglevel 2
-    if ( process.enve.LOGLEVEL == 2 ) {
-        console.log(`message: \`${message.content}\``);
-    }
+function logMessage (message) {
+  // check log level and log accordingly
+  // loglevel 1 and above
+  if (process.enve.LOGLEVEL >= 1) {
+    console.log(`${message.createdAt} - Deleted message from ${message.author.username}`)
+  }
+  // loglevel 2
+  if (process.enve.LOGLEVEL === 2) {
+    console.log(`message: \`${message.content}\``)
+  }
 }
 
-function checkMessage(message) {
-    // check for message sent by author
-    if (process.enve.USERS.includes(message.author.id)) {
-        // delete message
-        message.delete();
-        // log message
-        logMessage(message)
-    }
+function checkMessage (message) {
+  // check for message sent by author
+  if (process.enve.USERS.includes(message.author.id)) {
+    // delete message
+    message.delete()
+    // log message
+    logMessage(message)
+  }
 }
